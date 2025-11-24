c1d853740be220690702b887882105ec630b5fbb
oxxide216 <oxxide216@gmail.com>
2025-11-24 22:55:55 +0300

    Updated configs

--- install-config.sh
+++ install-config.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/sh
 
-mkdir -p $HOME/.config/emacs
 rm $HOME/dotfiles/home/.config/user-dirs.dirs $HOME/dotfiles/home/.config/user-dirs.locale
 sudo ln -s $HOME/dotfiles/etc/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf || true
 ln -s $HOME/dotfiles/home/.gitconfig $HOME/.gitconfig || true
 ln -s $HOME/dotfiles/home/.zshrc $HOME/.zshrc || true
 ln -s $HOME/dotfiles/home/.xinitrc $HOME/.xinitrc || true
 ln -s $HOME/dotfiles/home/.config/dunst $HOME/.config/dunst || true
-ln -s $HOME/dotfiles/home/.config/emacs/init.el $HOME/.config/emacs/init.el || true
 ln -s $HOME/dotfiles/home/.config/uwufetch $HOME/.config/uwufetch || true
 ln -s $HOME/dotfiles/home/.config/picom.conf $HOME/.config/picom.conf || true
 ln -s $HOME/dotfiles/home/.config/user-dirs.dirs $HOME/.config/user-dirs.dirs || true
--- install-progs.sh
+++ install-progs.sh
@@ -5,15 +5,15 @@ set -xe
 sudo pacman -Syu || true
 sudo pacman -S xorg xorg-xinit ly picom dunst feh brightnessctl bluez bluez-utils playerctl \
      pipewire pipewire-audio pipewire-jack pipewire-pulse wireplumber \
-     emacs firefox zsh zsh-autosuggestions zsh-syntax-highlighting \
+     firefox zsh zsh-autosuggestions zsh-syntax-highlighting \
      telegram-desktop discord flameshot zip unzip man-pages uwufetch make \
      nvidia nvidia-prime || true
 
-sudo systemctl enable ly bluetooth
-sudo systemctl start bluetooth.service
-systemctl --user --now enable pipewire pipewire-pulse
-
-chsh -s /usr/bin/zsh || true
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -ris
+cd ..
+sudo rm -rf yay
 
 sudo pacman -S wget
 
@@ -24,16 +24,22 @@ sudo mv ./trans /usr/local/bin
 sudo pacman -Rssn wget
 rm ~/.wget-hsts
 
+sudo systemctl enable ly bluetooth
+sudo systemctl start bluetooth.service
+systemctl --user --now enable pipewire pipewire-pulse
+
+chsh -s /usr/bin/zsh || true
+
 mkdir $HOME/src || true
-git clone https://github.com/vie216/dwm $HOME/src/dwm || true
+git clone https://github.com/oxxide216/dwm $HOME/src/dwm || true
 cd $HOME/src/dwm
 make
 sudo make install
-git clone https://github.com/vie216/st $HOME/src/st || true
+git clone https://github.com/oxxide216/st $HOME/src/st || true
 cd $HOME/src/st
 make
 sudo make install
-git clone https://github.com/vie216/slstatus $HOME/src/slstatus || true
+git clone https://github.com/oxxide216/slstatus $HOME/src/slstatus || true
 cd $HOME/src/slstatus
 make
 sudo make install
