c5bb7595982141ac45d83df88c540800bab69eab
oxxide216 <oxxide216@gmail.com>
2026-02-21 06:20:19 +0300

    Updated install-steam.sh script and made it full

--- install-steam.sh
+++ install-steam.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/sh
 
+sudo xbps-install void-repo-multilib-nonfree void-repo-multilib void-repo-nonfree
+sudo xbps-install -S
 sudo xbps-install steam libdrm-32bit nvidia-libs-32bit glibc-32bit mesa-32bit
