1deee3e111dbe4496b7d9b6292a5063483a9151a
oxxide216 <oxxide216@gmail.com>
2026-03-14 12:49:15 +0300

    Added xorg config for my touchpad

--- install-config-void.sh
+++ install-config-void.sh
@@ -12,6 +12,10 @@ ln -s $HOME/dotfiles/home/.config/user-dirs.locale $HOME/.config/user-dirs.local
 
 sudo ln -s $HOME/dotfiles/etc/environment /etc/environment || true
 
+sudo mkdir -p /etc/X11/xorg.conf.d
+sudo ln -s $HOME/dotfiles/etc/X11/xorg.conf.d/30-touchpad.conf \
+           /etc/X11/xorg.conf.d/30-touchpad.conf || true
+
 sudo ln -s /etc/sv/dbus /var/service/
 sudo sv up dbus
 
